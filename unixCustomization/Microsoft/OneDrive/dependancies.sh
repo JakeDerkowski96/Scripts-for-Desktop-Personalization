9d88d191fae7f626b3c209d4077bef91894e87a3
Jake Derkowski <45303650+JakeDerkowski96@users.noreply.github.com>
2023-05-01 11:32:21 -0500

    update

--- unixCustomization/Microsoft/OneDrive/dependancies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt install build-essential -y
-sudo apt install libcurl4-openssl-dev -y
-sudo apt install libsqlite3-dev -y
-sudo apt install pkg-config -y
-sudo apt install git -y
--- unixCustomization/scripts/debs.sh
+++
@@ -1,6 +0,0 @@
-# NOT DONE
-#script to get get and install deb packages from the internet
-
-# deb packages download
-
-# https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
